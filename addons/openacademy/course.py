006829bf0e1fdbb3c341dbe589c76ab4d0ecaa47
Miguel Paraíso <paraisomartin@gmail.com>
2015-08-10 22:35:25 +0200

    Updated Open Academy

--- addons/openacademy/course.py
+++ addons/openacademy/course.py
@@ -25,6 +25,20 @@ from openerp.osv import osv, fields
 class course(osv.Model):
     _name = "openacademy.course"
     
+    # Esta función me permite duplicar un curso
+    
+    def copy(self, cr, uid, id, default, context=None):
+        course = self.browse(cr, uid, id, context=context)
+        new_name = "Copy of %s" % course.name
+        # =like es el operador LIKE original de SQL
+        others_count = self.search(cr, uid, [('name','=like', new_name+'%')],count=True, context=context)
+        
+        if others_count > 0:
+            new_name = "%s (%s)" % (new_name, others_count+1)
+        default['name'] = new_name
+        return super(Course, self).copy(cr, uid, id , default, context=context)
+    
+    
     _columns = {
                 'name': fields.char(string="Title", size=64, required=True),
                 'description': fields.text(string="Description"),
@@ -52,7 +66,31 @@ class Session(osv.Model):
         for session in self.browse(cr, uid, ids, context=context):
             result[session.id] = self._get_taken_seats_percent(session.seats, session.attendee_ids)
         return result
+    
+    def onchange_taken_seats(self, cr, uid, ids, seats, attendee_ids):
+        attendee_records = self.resolve_o2m_commands_to_record_dicts(cr, uid, 'attendee_ids', attendee_ids, ['id'])
+        res = {
+               'value': {'taken_seats_percent': self._get_taken_seats_percent(seats, attendee_records)}
+               }
+        if seats < 0:
+            res['warning'] = {
+                'title': "Warning: problems",
+                'message': "You cannot have negative number of seats",
+                              }
+        elif seats < len(attendee_ids):
+            res['warning'] = {
+                'title': "Warning: problems",
+                'message': "You need more seats for this session ",
+                              }
+        return res
         
+    def _check_instructor_not_in_attendees(self, cr, uid, ids, context=None):
+        for session in self.browse(cr, uid, ids, context):
+            partners = [att.partner_id for att in session.attendee_ids]
+            if session.instructor_id and session.instructor_id in partners:
+                 return False
+        return True
+    
     
     _columns = {
                 'name': fields.char(string="Name", size=256, required=True),
@@ -64,11 +102,17 @@ class Session(osv.Model):
                 # 'cascade' destruye la sesión en caso de que course_id sea eliminado
                 'instructor_id': fields.many2one('res.partner', string="Instructor"),
                 # Ejercicio 3 domains 
-                #'instructor_id': fields.many2one('res.partner', string="Instructor", domain=[('instructor','=',True),('category_id.name','ilike','Teacher')]),
+               # 'instructor_id': fields.many2one('res.partner', string="Instructor", domain=[('instructor','=',True),('category_id.name','ilike','Teacher')],                
                 'course_id': fields.many2one('openacademy.course', ondelete='cascade', string="Course", required=True),
                 'attendee_ids': fields.one2many('openacademy.attendee','session_id', string="Attendees"), 
                 'taken_seats_percent': fields.function(_taken_seats_percent, type='float', string='Taken seats'),       
                 }
+    
+    _constraints = [
+        (_check_instructor_not_in_attendees,
+         "The instructor can not be also an attendee!",
+         ['instructor_id','attendee_ids']),
+                    ]
     
 
 class Attendee(osv.Model):
