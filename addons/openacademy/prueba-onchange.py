f2ce05ad7af3af8294037f9b5fec13f083f421de
Miguel Paraíso <paraisomartin@gmail.com>
2015-08-30 10:49:50 +0200

    Updated Open Academy

--- addons/openacademy/prueba-onchange.py
+++
@@ -1,8 +0,0 @@
-
-# EJemplo de un onchange
-
-def onchange_price(self, cr, uid, ids, unit_price, amount, context=None):
-    """ Cambia el precio cada vez que cambia el precio unitario o la cantidad total"""
-    return { 'value' : {'price' : unit_price * amount}}
-
-
